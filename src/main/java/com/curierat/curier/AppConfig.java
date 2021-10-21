cf4db4f50e47182fcf9643c98b9b0862cc9a7f89
maidacencoanastasia <maidacenco100@gmail.com>
2021-10-21 22:14:58 +0300

    liquibase

---
+++ src/main/java/com/curierat/curier/AppConfig.java
@@ -0,0 +1,16 @@
+package com.curierat.curier;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+//    @Bean
+//    public SpringLiquibase liquibase() {
+//        SpringLiquibase liquibase = new SpringLiquibase();
+//        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+//        liquibase.setDataSource(dataSource());
+//        return liquibase;
+//    }
+}
